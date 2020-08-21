b3b41e4858e8f0f58863fbef7fe521c5f7855e4d
Evhenyi-Petrenko <EvhenyiPetrenko@gmail.com>
2020-08-21 09:22:55 +0300

    Updates

---
+++ precache-manifest.0785fbe48a2e9d4a21709377e423d87d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab43e44e158223d78572611ff42993c",
+    "url": "/ToDo-React/index.html"
+  },
+  {
+    "revision": "4bfa3dfe37360e5766d8",
+    "url": "/ToDo-React/static/css/main.c65d53cf.chunk.css"
+  },
+  {
+    "revision": "611a6f860b8400b6b53d",
+    "url": "/ToDo-React/static/js/2.da677a82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-React/static/js/2.da677a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfa3dfe37360e5766d8",
+    "url": "/ToDo-React/static/js/main.38abe110.chunk.js"
+  },
+  {
+    "revision": "b38eefd2317a0903c9dd",
+    "url": "/ToDo-React/static/js/runtime-main.e7a7e5b3.js"
+  }
+]);
